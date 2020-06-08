20515ed4be98423c9b17486b3b299f1152ad1759
Seunghun Oh <seunghun.oh57@gmail.com>
2020-06-08 08:18:09 -0400

    Updates

--- precache-manifest.f7bf5953764072798f5b4b5ba96455d7.js
+++ precache-manifest.f7bf5953764072798f5b4b5ba96455d7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fda0d4945c5df4d22934109f9b186559",
+    "revision": "ab883c47dcfaf7c72d5fbc769ebc23a7",
     "url": "/typing-test/index.html"
   },
   {
-    "revision": "160921f13933ab4e2c44",
-    "url": "/typing-test/static/css/main.08c8e3a8.chunk.css"
+    "revision": "1968e45401f721f2ff85",
+    "url": "/typing-test/static/css/main.9745c647.chunk.css"
   },
   {
     "revision": "aa703394cc95cdf7b02b",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/typing-test/static/js/2.17cefaf0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "160921f13933ab4e2c44",
-    "url": "/typing-test/static/js/main.b0d76947.chunk.js"
+    "revision": "1968e45401f721f2ff85",
+    "url": "/typing-test/static/js/main.27fb4c10.chunk.js"
   },
   {
     "revision": "34553b6fdc1215b7a4f1",
